3a1d7b3eb21c841dee2c2580918367727cde8156
marukatze <kaffeekoenigg@gmail.com>
2025-12-01 15:03:02 +0300

    add time fields in Soul

--- app/src/main/model/Soul.java
+++ app/src/main/model/Soul.java
@@ -5,11 +5,18 @@ import main.utils.SoulState;
 public class Soul {
     private int sourceId;
     private int id;
+    private final double spawnTime;     // момент появления в мире Аида
+
+    private Double bufferEnterTime;     // когда попала в буфер
+    private Double serviceStartTime;    // когда попала на прибор
+    private Double serviceFinishTime;   // когда освобождена/переправлена
+
     private SoulState state;
 
-    public Soul(int sourceIdid, int id){
+    public Soul(int sourceIdid, int id, double spawnTime){
         this.sourceId = sourceIdid;
         this.id = id;
+        this.spawnTime = spawnTime;
         state = SoulState.NEW;
     }
 }
