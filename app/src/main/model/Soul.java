74c26d96599f85571ff1da1d57ca5b03ed8e3f6a
marukatze <kaffeekoenigg@gmail.com>
2025-12-01 14:25:26 +0300

    init Soul

---
+++ app/src/main/model/Soul.java
@@ -0,0 +1,11 @@
+package main.model;
+
+public class Soul {
+    private int sourceId;
+    private int id;
+
+    public Soul(int sourceIdid, int id){
+        this.sourceId = sourceIdid;
+        this.id = id;
+    }
+}
