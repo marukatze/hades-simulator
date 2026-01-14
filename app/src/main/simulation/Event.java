97fa7c0f511e419c38e1b1bd41b0f308e0b048e6
marukatze <kaffeekoenigg@gmail.com>
2026-01-14 13:59:33 +0300

    refactored Event

--- app/src/main/simulation/Event.java
+++ app/src/main/simulation/Event.java
@@ -2,26 +2,16 @@ package main.simulation;
 
 import main.model.Soul;
 
-public class Event implements Comparable<Event> {
+public class Event {
 
-    private final double time;       // время наступления события
-    private final EventType type;    // тип события
-    private final Soul soul;   // какая заявка участвует (если участвует)
-    private final Integer deviceId;  // какой прибор участвовал (если участвовал)
-
-    public Event(double time, EventType type) {
-        this(time, type, null, null);
-    }
+    private final double time;
+    private final EventType type;
+    private final Soul soul;
 
     public Event(double time, EventType type, Soul soul) {
-        this(time, type, soul, null);
-    }
-
-    public Event(double time, EventType type, Soul soul, Integer deviceId) {
         this.time = time;
         this.type = type;
         this.soul = soul;
-        this.deviceId = deviceId;
     }
 
     public double getTime() {
@@ -35,29 +25,4 @@ public class Event implements Comparable<Event> {
     public Soul getSoul() {
         return soul;
     }
-
-    public Integer getDeviceId() {
-        return deviceId;
-    }
-
-    /**
-     * Сравниваем события по времени.
-     * Это позволит использовать PriorityQueue<Event>
-     * как календарь событий — ближайшее событие всегда первое.
-     */
-    @Override
-    public int compareTo(Event other) {
-        return Double.compare(this.time, other.time);
-    }
-
-    @Override
-    public String toString() {
-        return "Event{" +
-                "time=" + time +
-                ", type=" + type +
-                ", Soul=" + (soul != null ? soul.getId() : "-") +
-                ", deviceId=" + (deviceId != null ? deviceId : "-") +
-                '}';
-    }
 }
-
