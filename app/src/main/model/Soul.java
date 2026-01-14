b74768e9516659ffa88fe33085f8e3cff80e51c6
marukatze <kaffeekoenigg@gmail.com>
2026-01-14 14:00:06 +0300

    refactored Soul

--- app/src/main/model/Soul.java
+++ app/src/main/model/Soul.java
@@ -1,54 +1,26 @@
 package main.model;
 
-import main.utils.SoulState;
+import main.utils.SoulStatus;
 
 public class Soul {
-    private int sourceId;
-    private String id;
-    private final double spawnTime;     // момент появления в мире Аида
 
-    private Double bufferEnterTime;     // когда попала в буфер
-    private Double serviceStartTime;    // когда попала на прибор
-    private Double serviceFinishTime;   // когда освобождена/переправлена
+    private final String id;
+    private SoulStatus state;
 
-    private SoulState state;
-
-    public Soul(int sourceId, String id, double spawnTime){
-        this.sourceId = sourceId;
+    public Soul(String id) {
         this.id = id;
-        this.spawnTime = spawnTime;
-        state = SoulState.NEW;
+        this.state = SoulStatus.NEW;
     }
 
     public String getId() {
         return id;
     }
 
-    public int getSourceId() {
-        return sourceId;
-    }
-
-    public double getSpawnTime() {
-        return spawnTime;
-    }
-
-    public SoulState getState() {
+    public SoulStatus getState() {
         return state;
     }
 
-    public void setState(SoulState newState) {
-        this.state = newState;
-    }
-
-    public void setBufferEnterTime(double time) {
-        this.bufferEnterTime = time;
-    }
-
-    public void setServiceStartTime(double time) {
-        this.serviceStartTime = time;
-    }
-
-    public void setServiceFinishTime(double time) {
-        this.serviceFinishTime = time;
+    public void setStatus(SoulStatus state) {
+        this.state = state;
     }
 }
