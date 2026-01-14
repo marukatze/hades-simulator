60e543803a9bc628d874fef36b2923ddfa3bd4ad
marukatze <kaffeekoenigg@gmail.com>
2026-01-14 13:59:45 +0300

    refactored EventType

--- app/src/main/simulation/EventType.java
+++ app/src/main/simulation/EventType.java
@@ -1,9 +1,8 @@
 package main.simulation;
 
 public enum EventType {
-    ARRIVAL,            // приход новой заявки
-    SERVICE_START,      // начало обслуживания
-    SERVICE_FINISH,     // завершение обслуживания
-    REPLACEMENT,        // вытеснение заявки из буфера (D1OO4)
-    BUFFER_INSERT       // запись заявки в буфер по кольцу (D1OЗ1)
+    SOUL_ARRIVED,
+    HADES_DECISION,
+    CHARON_FINISHED
 }
+
