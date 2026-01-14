a8ecac3b26cc01095335088bf57439bf9de54861
marukatze <kaffeekoenigg@gmail.com>
2026-01-14 13:59:28 +0300

    refactored Charon

--- app/src/main/model/Charon.java
+++ app/src/main/model/Charon.java
@@ -1,4 +1,40 @@
 package main.model;
 
+import main.simulation.Event;
+import main.simulation.EventType;
+import main.utils.SoulStatus;
+
 public class Charon {
+
+    private final String name;
+    private boolean busy = false;
+
+    public Charon(String name) {
+        this.name = name;
+    }
+
+    public boolean isBusy() {
+        return busy;
+    }
+
+    public Event transport(Soul soul, double currentTime) {
+        busy = true;
+        soul.setStatus(SoulStatus.TRANSPORTING);
+
+        double serviceTime = Math.random() * 5 + 1; // пока заглушка
+
+        return new Event(
+                currentTime + serviceTime,
+                EventType.CHARON_FINISHED,
+                soul
+        );
+    }
+
+    public void finish() {
+        busy = false;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
